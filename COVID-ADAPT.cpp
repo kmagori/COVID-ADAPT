13c8bb52b3ad516f4f5dde9a77e012af822ce7ce
kmagori <kmagori@gmail.com>
2020-11-28 21:41:43 -0800

    a

--- COVID-ADAPT.cpp
+++ COVID-ADAPT.cpp
@@ -23,6 +23,7 @@ class Person
     //Member functions
     void printname()
     {
+        // cout << "Person name is:" << identifier << " at xposition " << xposition << " and yposition " << yposition << " who is susceptible " << susceptible << " or exposed " << exposed << " or infected " << infected << " or infectious " << infectious << " or recovered " << recovered << " and is masked " << masked << " and is vaccinated " << vaccinated;
         cout << "Person name is:" << identifier << " at xposition " << xposition << " and yposition " << yposition << " who is susceptible " << susceptible << " or exposed " << exposed << " or infected " << infected << " or infectious " << infectious << " or recovered " << recovered << " and is masked " << masked << " and is vaccinated " << vaccinated << " and is age " << age;
     }
 };
@@ -60,59 +61,59 @@ int main()
     person1.vaccinated=false;
     person1.age=16;
 
-    places[1].identifier="Closet1";
-    places[1].xposition=1;
+    places[0].identifier="Closet1";
+    places[0].xposition=1;
+    places[0].yposition=1;
+    places[0].Virus_level=0;
+    places[0].Occupied=true;
+
+    places[1].identifier="Closet2";
+    places[1].xposition=2;
     places[1].yposition=1;
     places[1].Virus_level=0;
-    places[1].Occupied=true;
+    places[1].Occupied=false;
 
-    places[2].identifier="Closet2";
-    places[2].xposition=2;
+    places[2].identifier="Closet3";
+    places[2].xposition=3;
     places[2].yposition=1;
     places[2].Virus_level=0;
     places[2].Occupied=false;
 
-    places[3].identifier="Closet3";
-    places[3].xposition=3;
-    places[3].yposition=1;
+    places[3].identifier="Closet4";
+    places[3].xposition=1;
+    places[3].yposition=2;
     places[3].Virus_level=0;
     places[3].Occupied=false;
 
-    places[4].identifier="Closet4";
-    places[4].xposition=1;
+    places[4].identifier="Closet5";
+    places[4].xposition=2;
     places[4].yposition=2;
     places[4].Virus_level=0;
     places[4].Occupied=false;
 
-    places[5].identifier="Closet5";
-    places[5].xposition=2;
+    places[5].identifier="Closet6";
+    places[5].xposition=3;
     places[5].yposition=2;
     places[5].Virus_level=0;
     places[5].Occupied=false;
 
-    places[6].identifier="Closet6";
-    places[6].xposition=3;
-    places[6].yposition=2;
+    places[6].identifier="Closet7";
+    places[6].xposition=1;
+    places[6].yposition=3;
     places[6].Virus_level=0;
     places[6].Occupied=false;
 
-    places[7].identifier="Closet7";
-    places[7].xposition=1;
+    places[7].identifier="Closet8";
+    places[7].xposition=2;
     places[7].yposition=3;
     places[7].Virus_level=0;
     places[7].Occupied=false;
 
-    places[8].identifier="Closet8";
-    places[8].xposition=2;
+    places[8].identifier="Closet9";
+    places[8].xposition=3;
     places[8].yposition=3;
     places[8].Virus_level=0;
     places[8].Occupied=false;
-
-    places[9].identifier="Closet9";
-    places[9].xposition=3;
-    places[9].yposition=3;
-    places[9].Virus_level=0;
-    places[9].Occupied=false;
 
     //accessing member function
     person1.printname();
