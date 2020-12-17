5885874df4d73b55aabdfa3f81eaa1755acb732a
kmagori <kmagori@gmail.com>
2020-12-16 22:37:38 -0800

    recovery

--- COVID-ADAPT.cpp
+++ COVID-ADAPT.cpp
@@ -16,11 +16,12 @@ class Person
     string identifier;
     int xposition;
     int yposition;
-    bool susceptible;
-    bool exposed;
-    bool infected;
-    bool infectious;
-    bool recovered;
+    bool susceptible; //0
+    bool exposed;     //1
+    bool infected;    
+    bool infectious;  //2
+    bool recovered;   //3
+    int status;
     bool masked;
     bool vaccinated;
     int age;
@@ -28,11 +29,14 @@ class Person
     double movement_rate;
     double exposure_probability;
     double exposure_rate;
+    double exposure_time;
     double infectious_probability;
     double infectious_rate;
     double sum_prob;
-    double exposure_time;
-    double infectious_time;    
+    double infectious_time;  
+    double recovery_probability;
+    double recovery_rate;
+    double recovery_time;  
 
     //Member functions
     void printname()
@@ -59,6 +63,7 @@ class Person
         susceptible=false;
         exposed=true;
         infectious=false;
+        status=1;
         exposure_time=simtime;
         exposure_probability=0;
     }
@@ -68,8 +73,20 @@ class Person
         susceptible=false;
         exposed=false;
         infectious=true;
+        status=2;
         infectious_probability=0;
         infectious_time=simtime;
+    }
+
+    void recovering(double simtime)
+    {
+        susceptible=false;
+        exposed=false;
+        infectious=false;
+        recovered=true;
+        status=3;
+        recovery_probability=0;
+        recovery_time=simtime;
     }
 };
 
@@ -96,18 +113,21 @@ int main()
     //Declare an object
     srand (time(NULL));
     Person person[2];
-    Place places[9];
+    int gridsize=5;
+    Place places[gridsize*gridsize];
     double simtime,sum_prob;
-    int max_time=10000;
+    int max_time=100000;
     double steepness_exposure=1;
     int midpoint_exposure=50;
     double steepness_infectious=1;
     double midpoint_infectious=7200;
+    double steepness_recovery=1;
+    double midpoint_recovery=14400;
     int PlaceBefore,PlaceAfter,i,TogaPlace,EriPlace;
     ofstream record;
     record.open("record.csv");
     
-    record << "time,Toga_position,Eri_position,Eri_susceptible,Eri_exposed,Eri_infectious,Virus_Closet1,Virus_Closet2,Virus_Closet3,Virus_Closet4,Virus_Closet5,Virus_Closet6,Virus_Closet7,Virus_Closet8,Virus_Closet9";
+    record << "time,Toga_position,Eri_position,Toga_status,Eri_status,Virus_Closet1,Virus_Closet2,Virus_Closet3,Virus_Closet4,Virus_Closet5,Virus_Closet6,Virus_Closet7,Virus_Closet8,Virus_Closet9";
     record.close();
     //accessing data member
     person[0].identifier="Toga";
@@ -130,11 +150,15 @@ int main()
     person[0].infectious_probability=0;
     person[0].infectious_rate=0.1;
     person[0].infectious_time=0;
-    
+    person[0].recovery_probability=0;
+    person[0].recovery_rate=0.1;
+    person[0].recovery_time=-9999;
+    person[0].status=2;
+
     person[1].identifier="Eri";
-    person[1].xposition=3;
-    person[1].yposition=3;
-    person[1].location=8;
+    person[1].xposition=gridsize;
+    person[1].yposition=gridsize;
+    person[1].location=(gridsize*gridsize-1);
     person[1].susceptible=true;
     person[1].exposed=false;
     person[1].infected=false;
@@ -151,84 +175,88 @@ int main()
     person[1].infectious_probability=0;
     person[1].infectious_rate=0.1;
     person[1].infectious_time=-9999;
+    person[1].recovery_probability=0;
+    person[1].recovery_rate=0.1;
+    person[1].recovery_time=-9999;
+    person[1].status=0;
 
-    for (int i=0;i<3;i++)
+    for (int i=0;i<gridsize;i++)
     {
-        for (int j=0;j<3;j++)
+        for (int j=0;j<gridsize;j++)
         {
-            places[3*i+j].identifier="Closet" + to_string(3*i+j+1);
-            places[3*i+j].xposition=j+1;
-            places[3*i+j].yposition=i+1;
-            places[3*i+j].Virus_level=0;
-            places[3*i+j].Occupied=false;
-            places[3*i+j].person_in_there=-9999;
-            places[3*i+j].west_prob=25;
-            places[3*i+j].east_prob=25;
-            places[3*i+j].north_prob=25;
-            places[3*i+j].south_prob=25;
+            places[gridsize*i+j].identifier="Closet" + to_string(gridsize*i+j+1);
+            places[gridsize*i+j].xposition=j+1;
+            places[gridsize*i+j].yposition=i+1;
+            places[gridsize*i+j].Virus_level=0;
+            places[gridsize*i+j].Occupied=false;
+            places[gridsize*i+j].person_in_there=-9999;
+            places[gridsize*i+j].west_prob=25;
+            places[gridsize*i+j].east_prob=25;
+            places[gridsize*i+j].north_prob=25;
+            places[gridsize*i+j].south_prob=25;
             if (i==0&&j==0)
                 {
-                    places[3*i+j].west_prob=0;
-                    places[3*i+j].east_prob=50;
-                    places[3*i+j].north_prob=0;
-                    places[3*i+j].south_prob=50;
+                    places[gridsize*i+j].west_prob=0;
+                    places[gridsize*i+j].east_prob=50;
+                    places[gridsize*i+j].north_prob=0;
+                    places[gridsize*i+j].south_prob=50;
                 }
-            if (i==2&&j==0)
+            if (i==(gridsize-1)&&j==0)
                 {
-                    places[3*i+j].west_prob=0;
-                    places[3*i+j].east_prob=50;
-                    places[3*i+j].north_prob=50;
-                    places[3*i+j].south_prob=0;
+                    places[gridsize*i+j].west_prob=0;
+                    places[gridsize*i+j].east_prob=50;
+                    places[gridsize*i+j].north_prob=50;
+                    places[gridsize*i+j].south_prob=0;
                 }
-            if (i==0&&j==2)
+            if (i==0&&j==(gridsize-1))
                 {
-                    places[3*i+j].west_prob=50;
-                    places[3*i+j].east_prob=0;
-                    places[3*i+j].north_prob=0;
-                    places[3*i+j].south_prob=50;
+                    places[gridsize*i+j].west_prob=50;
+                    places[gridsize*i+j].east_prob=0;
+                    places[gridsize*i+j].north_prob=0;
+                    places[gridsize*i+j].south_prob=50;
                 }
-            if (i==2&&j==2)
+            if (i==(gridsize-1)&&j==(gridsize-1))
                 {
-                    places[3*i+j].west_prob=50;
-                    places[3*i+j].east_prob=0;
-                    places[3*i+j].north_prob=50;
-                    places[3*i+j].south_prob=0;
+                    places[gridsize*i+j].west_prob=50;
+                    places[gridsize*i+j].east_prob=0;
+                    places[gridsize*i+j].north_prob=50;
+                    places[gridsize*i+j].south_prob=0;
                 }
-            if (i==0&&j!=0&&j!=2)
+            if (i==0&&j!=0&&j!=(gridsize-1))
                 {
-                    places[3*i+j].west_prob=1.0/3*100;
-                    places[3*i+j].east_prob=1.0/3*100;
-                    places[3*i+j].north_prob=0;
-                    places[3*i+j].south_prob=1.0/3*100;
+                    places[gridsize*i+j].west_prob=1.0/3*100;
+                    places[gridsize*i+j].east_prob=1.0/3*100;
+                    places[gridsize*i+j].north_prob=0;
+                    places[gridsize*i+j].south_prob=1.0/3*100;
                 }
-            if (i!=0&&i!=2&&j==0)
+            if (i!=0&&i!=(gridsize-1)&&j==0)
                 {
-                    places[3*i+j].west_prob=0;
-                    places[3*i+j].east_prob=1.0/3*100;
-                    places[3*i+j].north_prob=1.0/3*100;
-                    places[3*i+j].south_prob=1.0/3*100;
+                    places[gridsize*i+j].west_prob=0;
+                    places[gridsize*i+j].east_prob=1.0/3*100;
+                    places[gridsize*i+j].north_prob=1.0/3*100;
+                    places[gridsize*i+j].south_prob=1.0/3*100;
                 }
-            if (i==2&&j!=0&&j!=2)
+            if (i==(gridsize-1)&&j!=0&&j!=(gridsize-1))
                 {
-                    places[3*i+j].west_prob=1.0/3*100;
-                    places[3*i+j].east_prob=1.0/3*100;
-                    places[3*i+j].north_prob=1.0/3*100;
-                    places[3*i+j].south_prob=0;
+                    places[gridsize*i+j].west_prob=1.0/3*100;
+                    places[gridsize*i+j].east_prob=1.0/3*100;
+                    places[gridsize*i+j].north_prob=1.0/3*100;
+                    places[gridsize*i+j].south_prob=0;
                 }
-            if (i!=0&&i!=2&&j==2)
+            if (i!=0&&i!=(gridsize-1)&&j==(gridsize-1))
                 {
-                    places[3*i+j].west_prob=1.0/3*100;
-                    places[3*i+j].east_prob=0;
-                    places[3*i+j].north_prob=1.0/3*100;
-                    places[3*i+j].south_prob=1.0/3*100;
+                    places[gridsize*i+j].west_prob=1.0/3*100;
+                    places[gridsize*i+j].east_prob=0;
+                    places[gridsize*i+j].north_prob=1.0/3*100;
+                    places[gridsize*i+j].south_prob=1.0/3*100;
                 }
         }
     }
 
     places[0].Occupied=true;
-    places[8].Occupied=true;
+    places[(gridsize*gridsize)-1].Occupied=true;
     places[0].person_in_there=0;
-    places[8].person_in_there=1;
+    places[(gridsize*gridsize)-1].person_in_there=1;
 
     //accessing member function
     //person1.printname();
@@ -251,9 +279,16 @@ int main()
             }
         }
 
+        //calculating probability of recovering
+        for (int i=0;i<2;i++) {
+            if (person[i].infectious) {
+            person[i].recovery_probability=1/(1+exp(-steepness_recovery*((simtime-person[i].infectious_time)-midpoint_recovery)));
+            }
+        }
+
         //Gillespie algorithm
         //calculating sum of probabilities across persons, with movement rate, exposure rate
-        for (int i=0;i<2;i++) {person[i].sum_prob=person[i].movement_rate+person[i].exposure_rate*person[i].exposure_probability+person[i].infectious_rate*person[i].infectious_probability;}
+        for (int i=0;i<2;i++) {person[i].sum_prob=person[i].movement_rate+person[i].exposure_rate*person[i].exposure_probability+person[i].infectious_rate*person[i].infectious_probability+person[i].recovery_rate*person[i].recovery_probability;}
 
         //calculating sum of probabilities overall
         sum_prob=0;
@@ -267,7 +302,7 @@ int main()
     //record << "\nThe time is " << simtime;
 
      //increase virus level by some value for all occupied places
-    for (i=0; i<=8; i++)
+    for (i=0; i<=(gridsize*gridsize-1); i++)
     {    
     if (places[i].Occupied)
         {
@@ -330,7 +365,11 @@ int main()
     else 
     if(v1<(person[current_person].movement_rate+(person[current_person].exposure_rate*person[current_person].exposure_probability)))
          person[current_person].exposure(simtime);
-         else person[current_person].becoming_infectious(simtime);
+         
+    else 
+    if (v1<(person[current_person].movement_rate+(person[current_person].exposure_rate*person[current_person].exposure_probability)+(person[current_person].infectious_rate*person[current_person].infectious_probability)))
+        person[current_person].becoming_infectious(simtime);
+        else person[current_person].recovering(simtime);
     //record << "\n" << person1.identifier << " is now at " << places[PlaceAfter].identifier << " which is at " << person1.xposition << " and " << person1.yposition;
     //std::printf("\nThe new position is %d and %d",person1.xposition,person1.yposition);   
 
@@ -361,7 +400,7 @@ int main()
     record.open("record.csv",std::fstream::app);
 
 
-    record << "\n" << simtime << "," << places[TogaPlace].identifier << "," << places[EriPlace].identifier << "," << person[1].susceptible << "," << person[1].exposed << "," << person[1].infectious << "," << places[0].Virus_level << "," << places[1].Virus_level << "," << places[2].Virus_level << "," << places[3].Virus_level << "," << places[4].Virus_level << "," << places[5].Virus_level << "," << places[6].Virus_level << "," << places[7].Virus_level << "," << places[8].Virus_level;
+    record << "\n" << simtime << "," << places[TogaPlace].identifier << "," << places[EriPlace].identifier << "," << person[0].status << "," << person[1].status << "," << places[0].Virus_level << "," << places[1].Virus_level << "," << places[2].Virus_level << "," << places[3].Virus_level << "," << places[4].Virus_level << "," << places[5].Virus_level << "," << places[6].Virus_level << "," << places[7].Virus_level << "," << places[8].Virus_level;
 
     record.close();
     }
