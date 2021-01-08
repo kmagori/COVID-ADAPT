ab3f3915a3bb3556af334bb6db6f66d3f5d7e8b9
kmagori <kmagori@gmail.com>
2021-01-07 19:30:51 -0800

    Delete plotting.R

--- plotting.R
+++
@@ -1,7 +0,0 @@
-COVIDADAPT=read.csv(file="record.csv",header=TRUE)
-viruslevels=COVIDADAPT[,6:30]
-current_viruslevels=matrix(as.numeric(viruslevels[1,]),nrow=5,ncol=5)
-library(raster)
-r=raster(ncol=5,nrow=5)
-values(r)=as.numeric(viruslevels[1953,])
-plot(r)
